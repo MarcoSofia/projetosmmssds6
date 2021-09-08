33bd0d166dfdfd474fc1601f915120b1fb270462
Marcos Sofia <marcos1970moreirasantos@gmail.com>
2021-09-08 18:57:57 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,37 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+
 
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary"> Olá mundo!</h1>
-     
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py=3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Taxa sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col px-6">
+            <h5  className="text-center text-secundary">Full vendas</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
